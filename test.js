5de4a1e161a43887d8d7764fb375314cc5f43c32
jahidul17 <jahidulislam.khan1234@gmail.com>
2025-09-01 23:55:59 +0600

    Happy coder

---
+++ test.js
@@ -0,0 +1,24 @@
+var namee = "Zahidul Islam"; //var support duplicate variable
+let nam = "Zahidul Islam"; //let does not support duplicate variable
+const names="Zahidul "; //const does not change value
+
+// console.log(nam.toLowerCase());
+// console.log(nam.toUpperCase());
+// console.log(nam.slice(2,3));
+// console.log(nam.length);
+// console.log(nam.charAt(0));
+
+
+// console.log("I love my country");
+// console.log(namee);
+// console.log(nam);
+// console.log(names);
+
+
+// const num=12.2355
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(5));
+
+var add=10+5;
+
+console.log("Sum of 10 and 5 is: " + add);
