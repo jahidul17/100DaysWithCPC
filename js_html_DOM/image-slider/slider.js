62dc7c98ccf74ea996db9e31161210deecf4556e
jahidul17 <jahidulislam.khan1234@gmail.com>
2025-09-15 15:08:26 +0600

    Happy coding

--- js_html_DOM/image-slider/slider.js
+++ js_html_DOM/image-slider/slider.js
@@ -29,6 +29,8 @@ function previousPhoto(){
 
 }
 
+// ----------- javascript bangla tutorial 54 : DOM | Changing CSS style dynamically-----------
+
 var paragraph = document.querySelector("#para");
 
 function addStyle(){
@@ -49,6 +51,8 @@ function removeStyle(){
     paragraph.style.borderRadius = "";
 }
 
+// -------- javascript bangla tutorial 55 : DOM | Event Listener-----------
+
 var listener = document.querySelector("#country");
 
 listener.addEventListener("click",eventListener);
@@ -68,3 +72,70 @@ lorem.addEventListener("mouseover", function(){
 lorem.addEventListener("mouseout", function(){
     lorem.classList.remove("my-style");
 });
+
+// ----------javascript bangla tutorial 56 : DOM | Event Listeners with multiple elements-----------
+
+// document.querySelectorAll(".my-button")[0].addEventListener("click", function(){
+//     var text=this.innerHTML;
+//     document.querySelector("h1").innerHTML=text + " is clicked";
+// });
+// document.querySelectorAll(".my-button")[1].addEventListener("click", function(){
+//     var text=this.innerHTML;
+//     document.querySelector("h1").innerHTML=text + " is clicked";
+// });
+// document.querySelectorAll(".my-button")[2].addEventListener("click", function(){
+//     var text=this.innerHTML;
+//     document.querySelector("h1").innerHTML=text + " is clicked";
+// })
+
+
+//Easy way to do the same thing using loop
+
+var len=document.querySelectorAll(".my-button").length;
+
+for(var i=0;i<len;i++){
+    document.querySelectorAll(".my-button")[i].addEventListener("click", function(){
+    var text=this.innerHTML;
+    document.querySelector("h1").innerHTML=text + " is clicked";
+})
+}
+
+//Or
+
+// document.querySelectorAll(".my-button").forEach(function(button){
+//     button.addEventListener("click", function(){
+//         var text=this.innerHTML;
+//         document.querySelector("h1").innerHTML=text + " is clicked";
+//     });
+// });
+
+// ----------javascript bangla tutorial 57 : DOM | how to play audio in javascript-----------
+
+var len2=document.querySelectorAll(".birds-btn").length;
+
+for(var i=0;i<len2;i++){
+    document.querySelectorAll(".birds-btn")[i].addEventListener("click", function(){
+        var text=this.innerHTML;
+        console.log(text + " is clicked");
+        audioPlay(text);
+    });
+}
+
+function audioPlay(text){
+    
+        switch(text){
+            case "Parrot":
+                var audio=new Audio("birds_sounds/parrot.mp3");
+                audio.play();
+                break;
+            case "Dove":
+                var audio=new Audio("birds_sounds/dove.mp3");
+                audio.play();
+                break;
+            case "Pigeon":
+                var audio=new Audio("birds_sounds/pigeon.mp3");
+                audio.play();
+                break;
+        }
+
+}
