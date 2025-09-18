e7dec75957f029d5a65d410b50fbed232aebf447
jahidul17 <jahidulislam.khan1234@gmail.com>
2025-09-18 23:36:06 +0600

    Easy Programming

---
+++ js_html_DOM/onsubmit event/index.js
@@ -0,0 +1,23 @@
+const form=document.querySelector("form");
+const name=form.querySelector("div #name");
+const email=form.querySelector("div #email");
+const password=form.querySelector("div #password");
+
+form.addEventListener('submit', formHandler);
+
+function formHandler(e){
+    e.preventDefault()
+    // console.log("name")
+
+    const userInfo={
+        name:name.value,
+        email:email.value,
+        password:password.value,
+
+    }
+    console.log(userInfo)
+
+}
+
+console.log(name);
+
